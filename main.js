845ba0b402377fdc59dab550ac4ef5f0d3054f4b
thuynvm1610 <thuynvm1610@gmail.com>
2025-04-06 17:55:08 +0700

    Homepage update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 window.onscroll = function () {
     const btn = document.getElementById("scrollToTopButton");
-    console.log("Scroll top:", document.documentElement.scrollTop);
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         btn.style.display = "block";
     } else {
