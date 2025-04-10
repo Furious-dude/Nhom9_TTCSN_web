e15b6b51a805b7c8d571170ab42ff539f9b0b969
thuynvm1610 <thuynvm1610@gmail.com>
2025-04-10 17:12:45 +0700

    Added responsive for header in homePage.html

--- main.js
+++ main.js
@@ -13,3 +13,45 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
+
+function subMenu1Display() {
+    var dl = document.getElementById("categoryForBelowLarge__item1-subMenu");
+    if (dl.style.display == "none") dl.style.display = "block";
+    else dl.style.display = "none";
+}
+
+function subMenu2Display() {
+    var dl = document.getElementById("categoryForBelowLarge__item2-subMenu");
+    if (dl.style.display == "none") dl.style.display = "block";
+    else dl.style.display = "none";
+}
+
+function subMenu3Display() {
+    var dl = document.getElementById("categoryForBelowLarge__item3-subMenu");
+    if (dl.style.display == "none") dl.style.display = "block";
+    else dl.style.display = "none";
+}
+
+function subMenu4Display() {
+    var dl = document.getElementById("categoryForBelowLarge__item4-subMenu");
+    if (dl.style.display == "none") dl.style.display = "block";
+    else dl.style.display = "none";
+}
+
+function subMenu5Display() {
+    var dl = document.getElementById("categoryForBelowLarge__item5-subMenu");
+    if (dl.style.display == "none") dl.style.display = "block";
+    else dl.style.display = "none";
+}
+
+function subMenu6Display() {
+    var dl = document.getElementById("categoryForBelowLarge__item6-subMenu");
+    if (dl.style.display == "none") dl.style.display = "block";
+    else dl.style.display = "none";
+}
+
+function subMenu8Display() {
+    var dl = document.getElementById("categoryForBelowLarge__item8-subMenu");
+    if (dl.style.display == "none") dl.style.display = "block";
+    else dl.style.display = "none";
+}
