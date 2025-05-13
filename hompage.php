422508164721dcd23e23692ee25900e08ab229a4
ML Hiep <100101scucide@gmail.com>
2025-05-13 09:03:02 +0700

    -added landing page -deploying

---
+++ hompage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homePage</title>
+</head>
+<body>
+    <h2>welcome to home</h2> 
+    <h2>phuc du</h2>
+    <h1>dit me cuoc song</h1>
+</body>
+</html>
